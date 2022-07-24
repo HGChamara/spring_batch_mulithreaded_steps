3442bf27592e85b48b8e228c54967b73263c1e6d
HGChamara <hgchamara1991@gmail.com>
2022-07-24 19:00:46 +0530

    first commit

---
+++ src/main/java/com/springbatch/multithreadedsteps/MultithreadedstepsApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.multithreadedsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadedstepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadedstepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbatch/multithreadedsteps/MultithreadedstepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatch.multithreadedsteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadedstepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
